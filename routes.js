7c853b7776f788ebb3a6973b5a03a19fed3f9777
BetimShala <betim_sh97@live.com>
2019-07-23 10:28:00 +0200

    index.html added

--- routes.js
+++ routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Controller = require("./Controller");
+const path = require('path');
 
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
 router.get("/page/:page", Controller.get);
 router.get("/show/page/:page", Controller.show);
 router.get("/download/page/:page", Controller.download);
-
 
 module.exports = router;
