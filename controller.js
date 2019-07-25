49a91329c2e7181ef4bfe1ad68a2539d1cc8f102
BetimShala <betim_sh97@live.com>
2019-07-25 14:36:53 +0200

    added validation for request page number

--- controller.js
+++ controller.js
@@ -1,11 +1,16 @@
 fs = require("fs");
-path = require("path")
+path = require("path");
 
 class Controller {
   static async get(req, res, next) {
     try {
-      const relativePath = `./quran-images/${req.params.page}.png`;
-      res.end(path.join(__dirname + `/quran-images/${req.params.page}.png`));
+      let pageNumber = req.params.page;
+      if (!Controller.validateRequest(pageNumber)) {
+        res.status(404).send("Page Not found");
+        return;
+      }
+
+      res.end(path.join(__dirname + `/quran-images/${pageNumber}.png`));
     } catch (ex) {
       return next(ex);
     }
@@ -13,7 +18,13 @@ class Controller {
 
   static async show(req, res, next) {
     try {
-      const file = `./quran-images/${req.params.page}.png`;
+      let pageNumber = req.params.page;
+      if (!Controller.validateRequest(pageNumber)) {
+        res.status(404).send("Page Not found");
+        return;
+      }
+
+      const file = `./quran-images/${pageNumber}.png`;
       var img = fs.readFileSync(file);
       res.writeHead(200, { "Content-Type": "image/png" });
       res.end(img, "binary");
@@ -24,11 +35,26 @@ class Controller {
 
   static async download(req, res, next) {
     try {
-      const file = `./quran-images/${req.params.page}.png`;
-      res.download(file); // Set disposition and send it.
+      let pageNumber = req.params.page;
+      if (!Controller.validateRequest(pageNumber)) {
+        res.status(404).send("Page Not found");
+        return;
+      }
+
+      const file = `./quran-images/${pageNumber}.png`;
+      res.download(file);
     } catch (ex) {
       return next(ex);
     }
   }
+
+  static validateRequest(pageNumber) {
+    pageNumber = parseInt(pageNumber);
+    if (pageNumber < 1 || pageNumber > 604) {
+      return false;
+    }
+    return true;
+  }
 }
+
 module.exports = Controller;
